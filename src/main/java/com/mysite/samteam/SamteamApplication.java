4fe7ebf252513e9c2d1e6291ba4f4eff733cd583
devops710 <toon710@gmail.com>
2024-03-11 18:02:48 +0900

    2403111801

---
+++ src/main/java/com/mysite/samteam/SamteamApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.samteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/samteam/SamteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.samteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
